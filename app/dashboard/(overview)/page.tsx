fdd3f2fcd7c4b7286aa8b6980f6e294bc60e4f7d
Marty Umlas <martyumlas@gmail.com>
2024-04-04 10:52:50 +0800

    streaming by grouping components

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,9 @@
 import { fetchLatestInvoices, fetchRevenue, fetchCardData } from "@/app/lib/data";
-import { Card } from "@/app/ui/dashboard/cards";
+import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import { lusitana } from "@/app/ui/fonts";
-import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
+import { CardSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 
 //this page is an async component
@@ -12,25 +12,29 @@ export default async function Page() {
   // const revenue = await fetchRevenue() remove for streaming using suspense
   // const latestInvoices = await fetchLatestInvoices() //wait for fetchRevenue() to finish
   // this is parellel data fetching, Promise.all(), Promise.allSettled()
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
-
+  // const {
+  //   numberOfInvoices,
+  //   numberOfCustomers,
+  //   totalPaidInvoices,
+  //   totalPendingInvoices,
+  // } = await fetchCardData();
+  //
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />  
+        {/* remove to grouping components */}
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
+        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
+        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
+        {/* <Card */}
+        {/*   title="Total Customers" */}
+        {/*   value={numberOfCustomers} */}
+        {/*   type="customers" */}
+        {/* />   */}
+        <Suspense fallback={<CardSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
